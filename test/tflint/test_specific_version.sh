1f4fe5162f245cc23124810c94fb982b16438694
Oscar Brouwer <HumanPrinter@users.noreply.github.com>
2026-05-21 07:50:00 +0200

    Use another version than the latest
    Co-authored-by: Arek Kalandyk <36413794+koralowiec@users.noreply.github.com>
    Signed-off-by: Oscar Brouwer <HumanPrinter@users.noreply.github.com>

--- test/tflint/test_specific_version.sh
+++ test/tflint/test_specific_version.sh
@@ -4,6 +4,6 @@ set -e
 
 source dev-container-features-test-lib
 
-check "tflint version is equal to 0.62.1" sh -c "tflint --version | grep '0.62.1'"
+check "tflint version is equal to 0.62.0" sh -c "tflint --version | grep '0.62.0'"
 
 reportResults
